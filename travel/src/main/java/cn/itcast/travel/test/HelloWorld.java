ad8ccd9511a73da01f3cc5c7dbfda6f79ea455f7
xiangyang <1738602087@qq.com>
2021-05-05 14:56:59 +0800

    李向阳

---
+++ travel/src/main/java/cn/itcast/travel/test/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName : HelloWorld  //类名
+ * @Description : 测试  //描述
+ * @Author : lxy //作者
+ * @Date: 2021-05-05 14:10  //时间
+ */
+package cn.itcast.travel.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+
+}
