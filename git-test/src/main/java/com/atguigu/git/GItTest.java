921ccb612f33e99bee76aee22ee253b979a1bcf0
xiangyang <1738602087@qq.com>
2021-12-27 14:32:04 +0800

    第一次提交

---
+++ git-test/src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author layne
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+    }
+}
